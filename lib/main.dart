95e085b2db9cadbb2b9b4ff96f3a6d149d6c71ab
brian <liar88828@gmail.com>
2024-10-18 15:00:44 +0700

    add bloc listener

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tutorial_bloc/counter_bloc/counter_bloc.dart';
@@ -37,49 +38,68 @@ class MyHome extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('My Home'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  style: IconButton.styleFrom(backgroundColor: Colors.blue),
-                  onPressed: () {
-                    context.read<CounterBloc>().add(CounterDecrementEvent());
-                  },
-                  icon: const Icon(Icons.remove),
-                ),
-                BlocBuilder<CounterBloc, CounterState>(
-                    buildWhen: (previous, current) {
-                  print(previous.counter);
-                  print(current.counter);
-                  return current.counter>=0;
-                  // return true; // will run
-                  // return false; // not run
-                }, builder: (context, state) {
-                  return Text(
-                    state.counter.toString(),
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  );
-                }),
-                IconButton(
-                  style: IconButton.styleFrom(backgroundColor: Colors.blue),
-                  onPressed: () {
-                    context.read<CounterBloc>().add(CounterIncrementEvent());
-                  },
-                  icon: const Icon(Icons.add),
-                )
-              ]
-                  .expand((widget) => [widget, const SizedBox(width: 10)])
-                  .toList(),
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BlocListener<CounterBloc, CounterState>(
+            listenWhen: (previous, current) {
+              // return false;// will not show the snackbar
+              return true;
+            },
+            listener: (context, state) {
+              if (state.counter >= 5) {
+                const snackBar = SnackBar(
+                  elevation: 0,
+                  behavior: SnackBarBehavior.floating,
+                  backgroundColor: Colors.transparent,
+                  content: AwesomeSnackbarContent(
+                      title: "Working!!",
+                      message: 'This example Bloc Listener!!',
+                      contentType: ContentType.success),
+                );
+                ScaffoldMessenger.of(context)
+                  ..hideCurrentSnackBar()
+                  ..showSnackBar(snackBar);
+              }
+            },
+            child: const Text('Bloc Listener'),
+          ),
+          const Text(
+            'You have pushed the button this many times:',
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                style: IconButton.styleFrom(backgroundColor: Colors.blue),
+                onPressed: () {
+                  context.read<CounterBloc>().add(CounterDecrementEvent());
+                },
+                icon: const Icon(Icons.remove),
+              ),
+              BlocBuilder<CounterBloc, CounterState>(
+                  buildWhen: (previous, current) {
+                print(previous.counter);
+                print(current.counter);
+                return current.counter >= 0;
+                // return true; // will run
+                // return false; // not run
+              }, builder: (context, state) {
+                return Text(
+                  state.counter.toString(),
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              }),
+              IconButton(
+                style: IconButton.styleFrom(backgroundColor: Colors.blue),
+                onPressed: () {
+                  context.read<CounterBloc>().add(CounterIncrementEvent());
+                },
+                icon: const Icon(Icons.add),
+              )
+            ].expand((widget) => [widget, const SizedBox(width: 10)]).toList(),
+          ),
+        ],
       ),
     );
   }
